061a118681dfee7ba7ebf3377026566c196a7601
Natalia <meryrua@gmail.com>
2025-03-22 15:03:32 -0400

    Another lab with Django classes

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -8,7 +8,12 @@ urlpatterns = [
     path(route='', view=views.popular_course_list, name='popular_course_list'),
     path('course/<int:course_id>/enroll/', views.enroll, name='enroll'),
     path('course/<int:course_id>/', views.course_details, name='course_details'),
-        
+    # Another lab for Django classes
+    '''
+    path(route='', view=views.CourseListView.as_view(), name='popular_course_list'),
+    path(route='course/<int:pk>/enroll/', view=views.EnrollView.as_view(), name='enroll'),
+    path(route='course/<int:pk>/', view=views.CourseDetailsView.as_view(), name='course_details'),'''
+     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
